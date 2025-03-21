c09adb48a4940683cac6e58d5ef84fba3bf9462b
samjrgodfrey <samjrgodfrey@gmail.com>
2025-03-21 11:08:27 +1100

    Use products.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,33 +1,3 @@
-const products = [
-  {
-    img: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    rating: {
-      stars: 4.5,
-      count: 87,
-    },
-    priceCents: 1090,
-  },
-  {
-    img: "images/products/intermediate-composite-basketball.jpg",
-    name: "Intermediate Size Basketball",
-    rating: {
-      stars: 4,
-      count: 127,
-    },
-    priceCents: 2095,
-  },
-  {
-    img: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    rating: {
-      stars: 4.5,
-      count: 56,
-    },
-    priceCents: 799,
-  },
-];
-
 let productsHTML = "";
 
 products.forEach((product) => {
@@ -36,7 +6,7 @@ products.forEach((product) => {
           <div class="product-image-container">
             <img
               class="product-image"
-              src="${product.img}"
+              src="${product.image}"
             />
           </div>
 
