1b343d7dc7316de5848e67b3000b4a8739a4fa3b
samjrgodfrey <samjrgodfrey@gmail.com>
2025-03-24 21:04:42 +1100

    Made changes to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export let cart = [
+  {
+    id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2,
+  },
+  {
+    id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+    quantity: 1,
+  },
+];
 
 export function addToCart(productId) {
   const qty = Number(
@@ -20,4 +29,16 @@ export function addToCart(productId) {
       quantity: qty,
     });
   }
+}
+
+export function removeFromCart(productId) {
+  const newCart = [];
+
+  cart.forEach((cartItem) => {
+    if (cartItem.productId !== productId) {
+      newCart.push(cartItem);
+    }
+  });
+
+  cart = newCart;
 }
